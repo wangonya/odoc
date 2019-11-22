83cb5d54516311cac9d576d3b37de54a9a94aa58
wangonya <kwangonya@gmail.com>
2019-11-22 11:59:35 +0300

    check if odoo dir

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
+import os
 import subprocess
 import click
+
+from halo import Halo
+
+spinner = Halo(text='Loading', spinner='dots', text_color='cyan')
 
 
 @click.group(invoke_without_command=True)
@@ -10,8 +15,26 @@ def cli(ctx):
         click.echo("Run odoc --help for options.")
 
 
+def check_odoo_dir():
+    """Checks if the current directory is an odoo root directory.
+
+    If the current directory contains an `odoo` module and has an
+    `odoo-bin` script, it's safe to assume that this is true.
+    """
+    spinner.start()
+
+    dir_items = [i for i in os.listdir('.')]
+    for i in dir_items:
+        if 'odoo' and 'odoo-bin' in i:
+            spinner.succeed("Yeap, we're in an odoo directory")
+            return True
+
+
 @cli.command()
 def generate():
     """Generate docs"""
-    subprocess.call(["pdoc", "-h"])
-    click.echo("Docs generated")
+    if not check_odoo_dir():
+        spinner.warn("This doesn't seem to be an odoo directory 🤔")
+
+    # subprocess.call(["pdoc", "-h"])
+    # click.echo("Docs generated")
