806f6545819953f6497f9625db51337182817e3a
wangonya <kwangonya@gmail.com>
2019-11-29 10:51:08 +0300

    trim unneeded methods

--- app/pdoc/cli.py
+++ app/pdoc/cli.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """pdoc's CLI interface and helper functions."""
 
-import argparse
-import ast
 import importlib
 import inspect
 import os
@@ -14,134 +12,9 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from typing import Sequence
 from warnings import warn
 
-import pdoc
-
-parser = argparse.ArgumentParser(
-    description="Automatically generate API docs for Python modules.",
-    epilog="Further documentation is available at <https://pdoc3.github.io/pdoc/doc>.",
-)
-aa = parser.add_argument
-mode_aa = parser.add_mutually_exclusive_group().add_argument
-
-aa(
-    '--version', action='version', version='%(prog)s ' + pdoc.__version__)
-aa(
-    "modules",
-    type=str,
-    metavar='MODULE',
-    nargs="+",
-    help="The Python module name. This may be an import path resolvable in "
-    "the current environment, or a file path to a Python module or "
-    "package.",
-)
-aa(
-    "-c", "--config",
-    type=str,
-    metavar='OPTION=VALUE',
-    action='append',
-    default=[],
-    help="Override template options. This is an alternative to using "
-         "a custom config.mako file in --template-dir. This option "
-         "can be specified multiple times.",
-)
-aa(
-    "--filter",
-    type=str,
-    metavar='STRING',
-    default=None,
-    help="Comma-separated list of filters. When specified, "
-         "only identifiers containing the specified string "
-         "will be shown in the output. Search is case sensitive. "
-         "Has no effect when --http is set.",
-)
-aa(
-    "-f", "--force",
-    action="store_true",
-    help="Overwrite any existing generated (--output-dir) files.",
-)
-mode_aa(
-    "--html",
-    action="store_true",
-    help="When set, the output will be HTML formatted.",
-)
-mode_aa(
-    "--pdf",
-    action="store_true",
-    help="When set, the specified modules will be printed to standard output, "
-         "formatted in Markdown-Extra, compatible with most "
-         "Markdown-(to-HTML-)to-PDF converters.",
-)
-aa(
-    "--html-dir",
-    type=str,
-    help=argparse.SUPPRESS,
-)
-aa(
-    "-o", "--output-dir",
-    type=str,
-    metavar='DIR',
-    help="The directory to output generated HTML/markdown files to "
-         "(default: ./html for --html).",
-)
-aa(
-    "--html-no-source",
-    action="store_true",
-    help=argparse.SUPPRESS,
-)
-aa(
-    "--overwrite",
-    action="store_true",
-    help=argparse.SUPPRESS,
-)
-aa(
-    "--external-links",
-    action="store_true",
-    help=argparse.SUPPRESS,
-)
-aa(
-    "--template-dir",
-    type=str,
-    metavar='DIR',
-    default=None,
-    help="Specify a directory containing Mako templates "
-         "(html.mako, text.mako, config.mako and/or any templates they include). "
-         "Alternatively, put your templates in $XDG_CONFIG_HOME/pdoc and "
-         "pdoc will automatically find them.",
-)
-aa(
-    "--link-prefix",
-    type=str,
-    help=argparse.SUPPRESS,
-)
-aa(
-    "--close-stdin",
-    action="store_true",
-    help="When set, stdin will be closed before importing, to account for "
-         "ill-behaved modules that block on stdin."
-)
+import app.pdoc as pdoc
 
 DEFAULT_HOST, DEFAULT_PORT = 'localhost', 8080
-
-
-def _check_host_port(s):
-    if s and ':' not in s:
-        raise argparse.ArgumentTypeError(
-            "'{}' doesn't match '[HOST]:[PORT]'. "
-            "Specify `--http :` to use default hostname and port.".format(s))
-    return s
-
-
-aa(
-    "--http",
-    default='',
-    type=_check_host_port,
-    metavar='HOST:PORT',
-    help="When set, pdoc will run as an HTTP server providing documentation "
-         "for specified modules. If you just want to use the default hostname "
-         "and port ({}:{}), set the parameter to :.".format(DEFAULT_HOST, DEFAULT_PORT),
-)
-
-args = argparse.Namespace()
 
 
 class WebDoc(BaseHTTPRequestHandler):
@@ -354,52 +227,14 @@ def _warn_deprecated(option, alternative='', use_config_mako=False):
 def main(_args=None):
     """ Command-line entry point """
     global args
-    args = _args or parser.parse_args()
+    args = _args
 
     warnings.simplefilter("once", DeprecationWarning)
-
-    if args.close_stdin:
-        sys.stdin.close()
 
     if (args.html or args.http) and not args.output_dir:
         args.output_dir = 'html'
 
-    if args.html_dir:
-        _warn_deprecated('--html-dir', '--output-dir')
-        args.output_dir = args.html_dir
-    if args.overwrite:
-        _warn_deprecated('--overwrite', '--force')
-        args.force = args.overwrite
-
     template_config = {}
-    for config_str in args.config:
-        try:
-            key, value = config_str.split('=', 1)
-            value = ast.literal_eval(value)
-            template_config[key] = value
-        except Exception:
-            raise ValueError(
-                'Error evaluating --config statement "{}". '
-                'Make sure string values are quoted?'
-                .format(config_str)
-            )
-
-    if args.html_no_source:
-        _warn_deprecated('--html-no-source', '-c show_source_code=False', True)
-        template_config['show_source_code'] = False
-    if args.link_prefix:
-        _warn_deprecated('--link-prefix', '-c link_prefix="foo"', True)
-        template_config['link_prefix'] = args.link_prefix
-    if args.external_links:
-        _warn_deprecated('--external-links')
-        template_config['external_links'] = True
-
-    if args.template_dir is not None:
-        if not path.isdir(args.template_dir):
-            print('Error: Template dir {!r} is not a directory'.format(args.template_dir),
-                  file=sys.stderr)
-            sys.exit(1)
-        pdoc.tpl_lookup.directories.insert(0, args.template_dir)
 
     # Support loading modules specified as python paths relative to cwd
     sys.path.append(os.getcwd())
@@ -439,11 +274,6 @@ def main(_args=None):
             sys.exit(0)
 
     docfilter = None
-    if args.filter and args.filter.strip():
-        def docfilter(obj, _filters=args.filter.strip().split(',')):
-            return any(f in obj.refname or
-                       isinstance(obj, pdoc.Class) and f in obj.doc
-                       for f in _filters)
 
     modules = [pdoc.Module(module, docfilter=docfilter)
                for module in args.modules]
@@ -494,7 +324,3 @@ or similar, at your own discretion.""",
             sys.stdout.write(module.text(**template_config))
             # Two blank lines between two modules' texts
             sys.stdout.write(os.linesep * (1 + 2 * int(module != modules[-1])))
-
-
-if __name__ == "__main__":
-    main(parser.parse_args())
